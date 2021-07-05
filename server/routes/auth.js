9793bab257957686907be637c5f5b723dd9e0faf
narayan <narayan3119@gmail.com>
2021-07-05 23:21:08 +0530

    added authorized routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,12 @@ const express = require("express");
 const User = require("../models/userSchema");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+const dotenv = require("dotenv")
 
+
+dotenv.config({path : "../.env"})
+const secret = process.env.JWT_SECRET;
 router.get("/", (req, res) => {
   res.send("no bro");
 });
@@ -34,6 +39,30 @@ router.post("/signup", (req, res) => {
   });
   //   console.log(req.body);
   //   res.status(200).json({ msg: "signup successfull" });
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: "Please fill all the details" });
+  }
+  User.findOne({ email: email }).then((user) => {
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+   bcrypt.compare(password, user.password).then((doMatch) => {
+    if (doMatch) {
+        // res.status(200).json({ msg: "User logged in successfully" });
+        const token = jwt.sign({_id:user._id}, secret)
+        res.json({token})
+      } else {
+        return res.status(400).json({ error: "Invalid credentials" });
+      }
+    })
+ 
+  }).catch((err) => {
+      console.log(err);
+  })
 });
 
 module.exports = router;
